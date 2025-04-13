c6b267588bff84f2e2060e5bdef34e4edd8034bb
Jim 2ha0 <Jim2ha0@aliyun.com>
2025-04-13 13:11:33 +0800

    Initial Commit

---
+++ src/main/scala/Welcome.scala
@@ -0,0 +1,5 @@
+object Welcome {
+  def main(args: Array[String]): Unit = {
+    println("Welcome to Jim's Algorithm Repository ^~^ ~~!")
+  }
+}
--- src/test/scala/lang/spec/ScalaLangSpec.scala
+++ src/test/scala/lang/spec/ScalaLangSpec.scala
@@ -1,12 +1,17 @@
+package lang.spec
+
 import org.scalatest.funsuite.AnyFunSuite
 
-class MyTest extends AnyFunSuite {
+class ScalaLangSpec extends AnyFunSuite {
+
   test("加法测试：1 + 1 应该等于 2") {
     assert(1 + 1 === 2)
+    assertResult(2)(1 + 1)
   }
 
-  test("字符串长度测试") {
+  test ("字符串长度测试") {
     val str = "hello"
     assert(str.length === 5)
+    assertResult(5)(str.length)
   }
 }
